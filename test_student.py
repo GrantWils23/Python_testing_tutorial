3b9569eccf0e16d389436f1767d78e88f768b2ee
Grant <GrantWils.23@googlemail.com>
2022-03-25 16:39:30 +0000

    add method functions alert_santa and email with tests

--- test_student.py
+++ test_student.py
@@ -10,5 +10,18 @@ class test_student(unittest.TestCase):
 
         self.assertEqual(student.full_name, "John Doe")
 
+    def test_alert_santa(self):
+        student = Student("John", "Doe")
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list, True)
+
+    def test_email_address(self):
+        student = Student("John", "Doe")
+        
+        self.assertEqual(student.email, "john.doe@email.com")
+
+
 if __name__ == "__main__":
     unittest.main()
+ 
