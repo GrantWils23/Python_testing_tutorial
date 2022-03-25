e7347b9e100103565770f46a1b6d92024cd4f3a6
Grant <GrantWils.23@googlemail.com>
2022-03-25 15:54:06 +0000

    add testing methods andd properties

---
+++ test_student.py
@@ -0,0 +1,14 @@
+''' test_student test file '''
+import unittest
+from student import Student
+
+
+class test_student(unittest.TestCase):
+
+    def test_full_name(self):
+        student = Student("John", "Doe")
+
+        self.assertEqual(student.full_name, "John Doe")
+
+if __name__ == "__main__":
+    unittest.main()
